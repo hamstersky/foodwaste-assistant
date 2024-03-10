3d988ca43155f545db0050863ffd8a8d36b3aba2
Dominik Chomicki <chomicki.dominik@gmail.com>
2024-03-10 11:33:14 +0100

    Store client in assistant's instance var

--- assistant.py
+++ assistant.py
@@ -5,10 +5,13 @@ from openai import NotFoundError, OpenAI
 
 class Assistant:
     id: int
+    client: OpenAI
 
     def __init__(self, client: OpenAI, assistant_id: Optional[str] = None):
+        self.client = client
+
         if assistant_id == None:
-            self.__create_new_assistant(client)
+            self.__create_new_assistant()
         else:
             try:
                 client.beta.assistants.retrieve(assistant_id)
@@ -17,9 +20,9 @@ class Assistant:
 
             self.id = assistant_id
 
-    def __create_new_assistant(self, client):
+    def __create_new_assistant(self):
         assistant_config = self.__load_assistant_config()
-        assistant = client.beta.assistants.create(**assistant_config)
+        assistant = self.client.beta.assistants.create(**assistant_config)
         self.id = assistant.id
 
     def __load_assistant_config(self):
