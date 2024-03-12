c21e8b1d4bbc5aacb7751b93c9ab184297085145
Dominik Chomicki <chomicki.dominik@gmail.com>
2024-03-12 21:13:49 +0100

    Add UI for getting store

--- main.py
+++ main.py
@@ -2,28 +2,30 @@ import openai
 import os
 from dotenv import load_dotenv
 from assistant import Assistant
+from foodwaste_api import FoodwasteAPI
 from thread import Thread
 from ui import UI
 
 load_dotenv()
 
 
-def setup(client, state):
+def setup(openai_client, state):
     if "thread" not in state:
-        state.thread = Thread(client)
+        state.thread = Thread(openai_client)
 
     if "assistant" not in state:
         assistant_id = os.getenv("ASSISTANT_ID")
-        state.assistant = Assistant(client, assistant_id)
+        state.assistant = Assistant(openai_client, assistant_id)
 
     if "messages" not in state:
         state.messages = []
 
 
 def main():
-    client = openai.OpenAI()
-    ui = UI(client)
-    setup(client, ui.state)
+    openai_client = openai.OpenAI()
+    foodwaste_client = FoodwasteAPI(os.getenv("SALLING_GROUP_API_TOKEN"))
+    ui = UI(openai_client, foodwaste_client)
+    setup(openai_client, ui.state)
     ui.initialize_state()
     ui.render_ui()
 
--- ui.py
+++ ui.py
@@ -2,17 +2,19 @@ import streamlit as st
 import time
 from assistant import Assistant
 from openai import OpenAI
+from foodwaste_api import FoodwasteAPI, Store
 from thread import Thread
 from run import Run
 
 
 class UI:
-    client: OpenAI
+    openai_client: OpenAI
     assistant: Assistant
     thread: Thread
 
-    def __init__(self, client: OpenAI):
-        self.client = client
+    def __init__(self, open_api: OpenAI, foodwaste_client: FoodwasteAPI):
+        self.openai_client = open_api
+        self.foodwaste_client = foodwaste_client
         self.state = st.session_state
 
     def initialize_state(self):
@@ -37,6 +39,30 @@ class UI:
                 yield letter + " "
                 time.sleep(0.02)
 
+    def __get_store_zipcode(self):
+        with st.form(key="zipcode"):
+            zipcode = st.text_input(
+                "Write the zip code where you want to find a store:"
+            )
+            submitted = st.form_submit_button(label="Submit")
+
+            if submitted:
+                st.session_state.zip = zipcode
+                st.rerun()
+
+    def __render_store_picker(self):
+        stores = self.foodwaste_client.get_stores(st.session_state.zip)
+        options = list(map(lambda store: store.id, stores))
+        stores_by_id = {store.id: store for store in stores}
+        format_func = (
+            lambda store_id: f"{stores_by_id[store_id].name}, {stores_by_id[store_id].address}"
+        )
+        selection = st.selectbox(
+            label="Select a specific store:", options=options, format_func=format_func
+        )
+        if selection:
+            st.session_state.store_id = selection
+
     def __get_prompt(self):
         if prompt := st.chat_input():
             self.__render_message("user", prompt)
@@ -46,7 +72,7 @@ class UI:
 
             run = None
             with st.spinner("Generating response..."):
-                run = Run(self.client, self.assistant.id, self.thread.id)
+                run = Run(self.openai_client, self.assistant.id, self.thread.id)
                 run.wait_for_run_completion()
 
             for response in self.thread.get_assistant_messages(before=message_id):
@@ -60,5 +86,10 @@ class UI:
                 )
 
     def render_ui(self):
-        self.__render_messages()
-        self.__get_prompt()
+        if "zip" not in st.session_state:
+            self.__get_store_zipcode()
+        elif "store_id" not in st.session_state:
+            self.__render_store_picker()
+        else:
+            self.__render_messages()
+            self.__get_prompt()
