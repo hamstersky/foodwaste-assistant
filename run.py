77f4a41e898456ce180526c69d6b80e377fca77b
Dominik Chomicki <chomicki.dominik@gmail.com>
2024-03-13 21:17:57 +0100

    Add function calling to run

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import json
 import time
 from openai import OpenAI
 from thread import Thread
+
+from foodwaste_api import FoodwasteAPI
 
 
 class Run:
@@ -15,32 +18,54 @@ class Run:
     def __init__(
         self,
         client: OpenAI,
+        foodwaste_client: FoodwasteAPI,
         assistant_id: int,
         thread_id: int,
     ):
         self.thread_id = thread_id
         self.client = client
+        self.foodwaste_client = foodwaste_client
         self.id = client.beta.threads.runs.create(
             assistant_id=assistant_id,
             thread_id=thread_id,
         ).id
 
-    def __get_run_status(self):
+    def __get_run(self):
         return self.client.beta.threads.runs.retrieve(
             thread_id=self.thread_id, run_id=self.id
-        ).status
+        )
 
     def wait_for_run_completion(self):
         retries = 0
         while retries < self.POLL_RETRIES:
-            if (status := self.__get_run_status()) in ["completed", "failed"]:
-                self.result = status
+            run = self.__get_run()
+            if run.status in ["completed", "failed"]:
+                self.result = run.status
                 return
+            elif run.status == "requires_action":
+                tool_outputs = self.call_functions(
+                    run.required_action.submit_tool_outputs.tool_calls
+                )
+
+                run = self.client.beta.threads.runs.submit_tool_outputs(
+                    thread_id=self.thread_id, run_id=run.id, tool_outputs=tool_outputs
+                )
 
             retries += 1
             time.sleep(self.POLL_INTERVAL)
 
         raise Exception("Run retrieval timeout.")
+
+    def call_functions(self, tool_calls):
+        tool_outputs = []
+        for tool_call in tool_calls:
+            function_name = tool_call.function.name
+            function_arguments = json.loads(tool_call.function.arguments)
+            func = getattr(self.foodwaste_client, function_name)
+            output = func(**function_arguments)
+            tool_outputs.append({"tool_call_id": tool_call.id, "output": str(output)})
+
+        return tool_outputs
 
     def is_run_successful(self):
         return self.result == "completed"
